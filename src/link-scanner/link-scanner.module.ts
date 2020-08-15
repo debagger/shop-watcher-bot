759570ce2d2673428222340c8f409b8e7b6ffdff
Mikhail Sokolov <debagger@gmail.com>
2020-08-15 12:22:38 +0500

    Refactor linkScanner with generator function

--- src/link-scanner/link-scanner.module.ts
+++ src/link-scanner/link-scanner.module.ts
@@ -5,8 +5,9 @@ import { ChatDataModule } from "src/chat-data/chat-data.module";
 import { SiteCrawlerModule } from "src/site-crawler/site-crawler.module";
 import { TelegramBotModule } from "src/telegram-bot/telegram-bot.module";
 import {CqrsModule} from '@nestjs/cqrs'
+import { LinkGenerator } from "./link-generator.provider";
 @Module({
   imports: [ChatDataModule, SiteCrawlerModule, CqrsModule],
-  providers: [LinkScannerService],
+  providers: [LinkScannerService, LinkGenerator],
 })
 export class LinkScannerModule {}
--- src/link-scanner/link-scanner.service.ts
+++ src/link-scanner/link-scanner.service.ts
@@ -1,9 +1,10 @@
 import { Injectable, OnApplicationBootstrap } from "@nestjs/common";
 import { SiteCrawlerService } from "src/site-crawler/site-crawler.service";
 import { ChatDataService } from "src/chat-data/chat-data.service";
-import { Link } from "src/file-db/chat-links.interface";
+import { Link, LinkCheckResult } from "src/file-db/chat-links.interface";
 import { EventBus } from "@nestjs/cqrs";
 import { NewSizeExist } from "./new-size-exist.event";
+import { LinkGenerator } from "./link-generator.provider";
 
 const sleep = (timeout: number) =>
   new Promise((resolve) => setTimeout(resolve, timeout));
@@ -13,9 +14,9 @@ export class LinkScannerService implements OnApplicationBootstrap {
   constructor(
     private Chat: ChatDataService,
     private spider: SiteCrawlerService,
+    private linkGenerator: LinkGenerator,
     private eventBus: EventBus
   ) {}
-
 
   async onApplicationBootstrap() {
     await this.runCheckAll();
@@ -23,36 +24,15 @@ export class LinkScannerService implements OnApplicationBootstrap {
 
   async runCheckAll() {
     const checkAll = async () => {
-      while (true) {
-        const chats = await this.Chat.getChatsIds();
-        for (const chatId of chats) {
-          await this.makeCheck(chatId);
+      for await(const {chatId, link} of this.linkGenerator){
+        try {
+          await this.checkLink(link, chatId);
+        } catch (error) {
+          
         }
-        const delay = (Math.random() + 1) * 10 * 60 * 1000;
-        console.log(
-          `Wait ${Math.floor(delay / 1000)} sec before next iteration.`
-        );
-        await sleep(delay);
       }
-    };
-
-    setTimeout(checkAll, 1000);
-  }
-
-  async makeCheck(chatId: number) {
-    const chat = await this.Chat.getChat(chatId);
-    const links = chat.links;
-
-    const zaraLinks = Object.keys(links).filter((i) =>
-      i.startsWith("https://www.zara.com/")
-    );
-
-    for (const link of zaraLinks) {
-      try {
-        await this.checkLink(link, chatId);
-      } catch (err) {}
-      await sleep(Math.random() * 10 * 1000 + 1000);
     }
+    setTimeout(checkAll, 1000);
   }
 
   async checkLink(link: string, chatId: number) {
@@ -67,23 +47,28 @@ export class LinkScannerService implements OnApplicationBootstrap {
         linkData.trackFor &&
         linkData.trackFor.length > 0
       ) {
-        const oldSizeExist = linkData.lastCheckResult.sizes
-          .filter((s) => linkData.trackFor?.includes(s.size) && !s.disabled)
-          .map((s) => s.size);
-
-        const sizeExist = newData.sizes
-          .filter(
-            (s) =>
-              !oldSizeExist.includes(s.size) &&
-              linkData.trackFor?.includes(s.size) &&
-              !s.disabled
-          )
-          .map((s) => s.size);
+        const sizeExist = this.newSizes(linkData, newData);
         if (sizeExist.length > 0) {
           this.eventBus.publish(new NewSizeExist(chatId, link, sizeExist));
         }
       }
       linkData.lastCheckResult = newData;
     }
+  }
+
+  private newSizes(linkData: Link, newData: LinkCheckResult) {
+    const oldSizeExist = linkData.lastCheckResult.sizes
+      .filter((s) => linkData.trackFor?.includes(s.size) && !s.disabled)
+      .map((s) => s.size);
+
+    const sizeExist = newData.sizes
+      .filter(
+        (s) =>
+          !oldSizeExist.includes(s.size) &&
+          linkData.trackFor?.includes(s.size) &&
+          !s.disabled
+      )
+      .map((s) => s.size);
+    return sizeExist;
   }
 }
