ef74e68bdd419d16aefbfee236e6f038fba385f7
Mikhail Sokolov <debagger@gmail.com>
2021-12-21 08:30:04 +0500

    Test for github hook

--- src/proxy-list/proxy-list.module.ts
+++ src/proxy-list/proxy-list.module.ts
@@ -1,20 +1,40 @@
-import { Module, OnModuleInit } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { ProxyListSource, ProxyListUpdate, Proxy, ProxyTestRun, ProxyTestType } from 'src/entities';
-import { ProxyListSourcesService } from './proxy-list-sources/proxy-list-sources.service';
-import { ProxyListUpdaterService } from './proxy-list-updater/proxy-list-updater.service';
-import { ProxyTesterService } from './proxy-tester/proxy-tester.service';
+import { Module, OnModuleInit } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import {
+  ProxyListSource,
+  ProxyListUpdate,
+  Proxy,
+  ProxyTestRun,
+  ProxyTestType,
+} from "src/entities";
+import { ProxyListSourcesService } from "./proxy-list-sources/proxy-list-sources.service";
+import { ProxyListUpdaterService } from "./proxy-list-updater/proxy-list-updater.service";
+import { ProxyTesterService } from "./proxy-tester/proxy-tester.service";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ProxyListSource, ProxyListUpdate, Proxy, ProxyTestRun, ProxyTestType])],
-  providers: [ProxyListSourcesService, ProxyListUpdaterService, ProxyTesterService]
+  imports: [
+    TypeOrmModule.forFeature([
+      ProxyListSource,
+      ProxyListUpdate,
+      Proxy,
+      ProxyTestRun,
+      ProxyTestType,
+    ]),
+  ],
+  providers: [
+    ProxyListSourcesService,
+    ProxyListUpdaterService,
+    ProxyTesterService,
+  ],
 })
 export class ProxyListModule implements OnModuleInit {
-  constructor(private proxyUpdater: ProxyListUpdaterService, 
-    private proxyTester: ProxyTesterService){}
-  async onModuleInit(){
+  constructor(
+    private proxyUpdater: ProxyListUpdaterService,
+    private proxyTester: ProxyTesterService
+  ) {}
+  async onModuleInit() {
     // await this.proxyUpdater.updateSource(1)
-    await this.proxyUpdater.updateAllSources()
-     await this.proxyTester.checkAllProxies()
+    // await this.proxyUpdater.updateAllSources();
+    // await this.proxyTester.checkAllProxies();
   }
- }
+}
