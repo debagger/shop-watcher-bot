263cfd83385bece4021f59afe3e1b464c54b3bd2
debagger <debagger@gmail.com>
2021-12-22 04:31:00 +0000

    Update logging for github hook

--- src/main.ts
+++ src/main.ts
@@ -10,12 +10,16 @@ import * as hookshot from "hookshot"
 async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule);
-  app.useLogger(app.get(Logger))
+  const logger = app.get(Logger) 
+  app.useLogger(logger)
   const adapter = app.getHttpAdapter();
   adapter.use(cookie())
   adapter.use("/hookshot",
     hookshot('refs/heads/master', 'git pull && npm run build && pm2 restart app')
-      .on('push', (info) => console.log('ref ' + info.ref + ' was pushed.')))
+      .on('push', (info) => {
+        logger.log('ref ' + info.ref + ' was pushed.')
+        console.dir(info, { compact: 6, depth: 4, breakLength: 255, colors: true })
+      }))
   const port = env.PORT ? env.port : 3001
 
   await app.listen(port);
