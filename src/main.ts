f2f029664b630fa4178b254f4f47ec3514f4dcbf
debagger <debagger@gmail.com>
2021-12-22 04:49:55 +0000

    Update github hook script

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,27 @@
-import { Logger } from "nestjs-pino"
+import { Logger, PinoLogger } from "nestjs-pino"
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from "dotenv"
 import * as cookie from "cookie-parser"
 import { env } from "process"
 import * as hookshot from "hookshot"
+import { spawn } from 'child_process'
 
 
 async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule);
-  const logger = app.get(Logger) 
+  const logger = app.get(Logger)
   app.useLogger(logger)
   const adapter = app.getHttpAdapter();
   adapter.use(cookie())
   adapter.use("/hookshot",
-    hookshot('refs/heads/master', 'git pull && npm run build && pm2 restart app')
-      .on('push', (info) => {
-        logger.log('ref ' + info.ref + ' was pushed.')
-        console.dir(info, { compact: 6, depth: 4, breakLength: 255, colors: true })
-      }))
+    hookshot('refs/heads/master', function (info) {
+      const shell = process.env.SHELL;
+      const args = ['-c', "'git pull && npm run build'"];
+      logger.log(`ref ${info.ref} was pushed. Run ${shell} with ${args}`)
+      spawn(shell, args, { stdio: 'inherit' })
+    }))
   const port = env.PORT ? env.port : 3001
 
   await app.listen(port);
