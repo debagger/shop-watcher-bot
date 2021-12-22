e2d6b50f70479b8ed00e76038638fad66c312985
debagger <debagger@gmail.com>
2021-12-22 05:16:47 +0000

    Remove unnessesry single quotes

--- src/main.ts
+++ src/main.ts
@@ -18,9 +18,9 @@ async function bootstrap() {
   adapter.use("/hookshot",
     hookshot('refs/heads/master', function (info) {
       const shell = process.env.SHELL;
-      const args = ['-c', "'git pull && npm run build'"];
+      const args = ['-c', "git pull && npm run build"];
       logger.log(`ref ${info.ref} was pushed. Run ${shell} with ${args}`)
-      spawn(shell, args, { stdio: 'inherit' })
+      spawn(shell, args, { stdio: 'inherit', detached: true })
     }))
   const port = env.PORT ? env.port : 3001
 
