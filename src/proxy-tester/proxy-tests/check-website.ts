b60342545d89aeafd70d53c26554e391bcc6c272
debagger <debagger@gmail.com>
2022-02-07 19:05:18 +0000

    Add user-agent header

--- src/proxy-tester/proxy-tests/check-website.ts
+++ src/proxy-tester/proxy-tests/check-website.ts
@@ -20,6 +20,8 @@ export async function checkWebsite(
   const start = performance.now();
 
   const headers = {
+    "User-Agent":
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36",
     accept:
       "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
     "accept-language": "ru-RU,ru;q=0.9",
@@ -32,7 +34,7 @@ export async function checkWebsite(
     const response = await axios.get(url, {
       httpsAgent,
       timeout,
-      headers
+      headers,
     });
 
     result.duration_ms = performance.now() - start;
